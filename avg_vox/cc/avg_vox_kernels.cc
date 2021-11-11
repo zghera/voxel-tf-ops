058e3701e6c3dea46ab8aae77e8139fedc82b4f8
Zach Ghera <zpghera00@gmail.com>
2021-11-11 11:23:11 -0500

    FImplement OpKernel::Compute with resolution as an input.
    I think it is not feasible to use resolution as in input because we need it to set the output shapes (r**3). So I am going to try with resolution as an Attr instead.

--- avg_vox/cc/avg_vox_kernels.cc
+++ avg_vox/cc/avg_vox_kernels.cc
@@ -4,9 +4,6 @@
 
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
-// #include "tensorflow/core/framework/tensor.h"
-// #include "tensorflow/core/framework/tensor_shape.h"
-// #include "tensorflow/core/framework/tensor_types.h"
 #include "tensorflow/core/lib/core/status.h"
 
 namespace tensorflow {
@@ -19,31 +16,31 @@ typedef Eigen::GpuDevice GPUDevice;
 
 REGISTER_OP("AvgVoxForward")
     .Input("features: float")
-    // .Input("coords: int32")
-    // .Input("resolution: int32")
+    .Input("coords: int32")
+    .Input("resolution: int32")
     .Output("out: float")
-    // .Output("ind: int32");
-    // .Output("cnt: int32");
+    .Output("ind: int32")
+    .Output("cnt: int32")
     .SetShapeFn([](InferenceContext* c) {
       // Input rank assertions
       ShapeHandle input;
       TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 3, &input));
-      // TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 3, &input));
-      // TF_RETURN_IF_ERROR(c->WithRank(c->input(2), 0, &input));
+      TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 3, &input));
+      TF_RETURN_IF_ERROR(c->WithRank(c->input(2), 0, &input));
 
-      // // Get (resolution ** 3) as a dimension handle
-      // DimensionHandle s;
-      // TF_RETURN_IF_ERROR(c->MakeDimForScalarInput(2, &s));
-      // TF_RETURN_IF_ERROR(c->Multiply(s, s, &s));
-      // TF_RETURN_IF_ERROR(c->Multiply(s, s, &s));
+      // Get (resolution ** 3) as a dimension handle
+      DimensionHandle s;
+      TF_RETURN_IF_ERROR(c->MakeDimForScalarInput(2, &s));
+      TF_RETURN_IF_ERROR(c->Multiply(s, s, &s));
+      TF_RETURN_IF_ERROR(c->Multiply(s, s, &s));
 
-      // // Specifying output shapes
-      // ShapeHandle outShape = c->MakeShape(
-      //     {c->dim(c->input(0),0), c->Dim(c->input(0),1), s});
-      // c->set_output(0, outShape);
-      // c->set_output(1, c->Matrix(c->dim(c->input(0),0),
-      //                            c->dim(c->input(0),2) ));
-      // c->set_output(2, c->Matrix(c->dim(c->input(0),0), s));
+      // Specifying output shapes
+      ShapeHandle outShape = c->MakeShape(
+          {c->Dim(c->input(0),0), c->Dim(c->input(0),1), s});
+      c->set_output(0, outShape);
+      c->set_output(1, c->Matrix(c->Dim(c->input(0),0),
+                                 c->Dim(c->input(0),2) ));
+      c->set_output(2, c->Matrix(c->Dim(c->input(0),0), s));
 
       return Status::OK();
 });
@@ -56,28 +53,30 @@ class AvgVoxForwardOp : public OpKernel {
   explicit AvgVoxForwardOp(OpKernelConstruction* context) : OpKernel(context) {}
 
   void Compute(OpKernelContext* context) override {
-    // // Grab the input tensor
-    const Tensor& input_tensor = context->input(0);
+    // Grab the input tensors
+    const Tensor& features = context->input(0);
+    const Tensor& coords = context->input(1);
+    const Tensor& resolutionTensor = context->input(2);
+    auto resolutionFlat = resolutionTensor.flat<int32>();
 
-    // // Create an output tensor
-    Tensor* output_tensor = NULL;
+    // Get shape and resolution integers
+    int batches   = features.shape().dim_size(0);
+    int channels  = features.shape().dim_size(1);;
+    int numPoints = features.shape().dim_size(2);;
+    int res = resolutionFlat(0);
+    int res2 = res * res;
+    int res3 = res * res2;;
 
-    //   // Get (resolution ** 3) as a dimension handle
-    //   DimensionHandle s;
-    //   TF_RETURN_IF_ERROR(c->MakeDimForScalarInput(2, &s));
-    //   TF_RETURN_IF_ERROR(c->Multiply(s, s, &s));
-    //   TF_RETURN_IF_ERROR(c->Multiply(s, s, &s));
-
-    //   // Specifying output shapes
-    //   ShapeHandle outShape = c->MakeShape(
-    //       {c->dim(c->input(0),0), c->Dim(c->input(0),1), s});
-    //   c->set_output(0, outShape);
-    //   c->set_output(1, c->Matrix(c->dim(c->input(0),0),
-    //                              c->dim(c->input(0),2) ));
-    //   c->set_output(2, c->Matrix(c->dim(c->input(0),0), s));
-
-    OP_REQUIRES_OK(context, context->allocate_output(
-      0, input_tensor.shape(), &output_tensor));
+    // Create output tensors
+    Tensor* out = NULL;
+    Tensor* ind = NULL;
+    Tensor* cnt = NULL;
+    TensorShape outShape{batches, channels, res3};
+    TensorShape indShape{batches, numPoints};
+    TensorShape cntShape{batches, res3};
+    OP_REQUIRES_OK(context, context->allocate_output(0, outShape, &out));
+    OP_REQUIRES_OK(context, context->allocate_output(1, indShape, &ind));
+    OP_REQUIRES_OK(context, context->allocate_output(2, cntShape, &cnt));
 
     // Do the computation.
     OP_REQUIRES_OK(context, AvgVoxForwardKernelLauncher(
