92fd681e456b2a382a853ed72dab5411bb434ce2
Zach Ghera <zpghera00@gmail.com>
2021-11-14 20:27:59 -0500

    Condense constants in avg_vox tests.

--- avg_vox/python/ops/avg_vox_ops_test.py
+++ avg_vox/python/ops/avg_vox_ops_test.py
@@ -22,63 +22,40 @@ class AvgVoxTest(tf.test.TestCase):
   def test_avg_voxelize_forward(self):
     # fmt: off
     with tf.device("/device:GPU:0"):
-      # [B, C, N] = [2, 5, 4]
-      features = tf.constant([
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-      ], dtype=tf.float32)
-      # [B, 3, N] = [2, 3, 4]
-      coords = tf.constant([
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-      ], dtype=tf.int32)
-      resolution = tf.constant(self.R)
-      out, ind, cnt = avg_voxelize_forward(features, coords, resolution)
-
       # [B, C, R**3] = [2, 5, 64]
-      expected_out = tf.constant([
-        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+      expected_out = tf.constant([[
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,]]
-        ], dtype=tf.float32)
-      expected_out = tf.repeat(expected_out, 2, axis=0)
+        ]], dtype=tf.float32)
+      expected_out = tf.repeat(tf.repeat(expected_out, 5, axis=1), 2, axis=0)
+
       # [B, R**3] = [2, 64]
       expected_cnt = tf.constant([
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
       ], dtype=tf.int32)
+      expected_cnt = tf.repeat(expected_cnt, 2, axis=0)
+
       # [B, N] = [2, 4]
       expected_ind = tf.constant([
         [21, 42, 63, 84], [21, 42, 63, 84]
       ], dtype=tf.int32)
+
+
+      # [B, C, N] = [2, 5, 4]
+      features = tf.constant([[[1, 2, 3, 4]]], dtype=tf.float32)
+      features = tf.repeat(tf.repeat(features, 5, axis=1), 2, axis=0)
+
+      # [B, 3, N] = [2, 3, 4]
+      coords = tf.constant([[[1, 2, 3, 4]]], dtype=tf.int32)
+      coords = tf.repeat(tf.repeat(coords, 3, axis=1), 2, axis=0)
+
+      resolution = tf.constant(self.R)
+
+
+      out, ind, cnt = avg_voxelize_forward(features, coords, resolution)
 
       self.assertAllClose(out, expected_out)
       self.assertAllClose(cnt, expected_cnt)
@@ -89,28 +66,22 @@ class AvgVoxTest(tf.test.TestCase):
     # fmt: off
     with tf.device("/device:GPU:0"):
       # [B, C, N] = [2, 5, 4]
-      features = tf.constant([
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-      ], dtype=tf.float32)
+      expected_features_grad = tf.constant([[[0, 0.003125, 0.00625, 0]]])
+      expected_features_grad = tf.repeat(
+          tf.repeat(expected_features_grad, 5, axis=1), 2, axis=0)
+
+
+      # [B, C, N] = [2, 5, 4]
+      features = tf.constant([[[1, 2, 3, 4]]], dtype=tf.float32)
+      features = tf.repeat(tf.repeat(features, 5, axis=1), 2, axis=0)
+
       # [B, 3, N] = [2, 3, 4]
-      coords = tf.constant([
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-        [[1, 2, 3, 4],
-         [1, 2, 3, 4],
-         [1, 2, 3, 4]],
-      ], dtype=tf.int32)
-      resolution = tf.constant(4)
+      coords = tf.constant([[[1, 2, 3, 4]]], dtype=tf.int32)
+      coords = tf.repeat(tf.repeat(coords, 3, axis=1), 2, axis=0)
+
+      resolution = tf.constant(self.R)
+
+
       out, ind, cnt = avg_voxelize_forward(features, coords, resolution)
 
       mse = tf.keras.losses.MeanSquaredError()
@@ -121,19 +92,6 @@ class AvgVoxTest(tf.test.TestCase):
         dL_dout = tape.gradient(loss, out)
 
       features_grad = avg_voxelize_backward(dL_dout, ind, cnt)
-      # [B, C, N] = [2, 5, 4]
-      expected_features_grad = tf.constant([
-        [[0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0]],
-        [[0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0],
-         [0,  0.003125, 0.00625,  0]]
-      ])
 
       self.assertAllClose(features_grad, expected_features_grad)
     # fmt: on
